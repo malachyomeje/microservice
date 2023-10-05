45c7a836d42d117812d07edd7b04256e1a26b28d
malachy <malachyomeje@gmail.com>
2023-10-05 13:22:29 +0100

    student commit

--- student/src/main/java/com/malachy/student/repository/StudentsRepository.java
+++ student/src/main/java/com/malachy/student/repository/StudentsRepository.java
@@ -3,9 +3,12 @@ package com.malachy.student.repository;
 import com.malachy.student.model.Students;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentsRepository extends JpaRepository<Students, Long>{
 
     Optional<Students> findByEmail(String email);
+
+    List<Students>findAllBySchoolId(Long schoolId);
 }
--- student/src/main/java/com/malachy/student/studentService/StudentService.java
+++ student/src/main/java/com/malachy/student/studentService/StudentService.java
@@ -1,6 +1,7 @@
 package com.malachy.student.studentService;
 
 import com.malachy.student.dto.request.StudentDto;
+import com.malachy.student.dto.request.StudentWithSchoolDto;
 import com.malachy.student.dto.response.ApiResponse;
 
 import java.util.List;
@@ -9,4 +10,8 @@ public interface StudentService {
     ApiResponse registerStudent(StudentDto studentDto);
 
     List<StudentDto> findAllStudent();
+
+   // ApiResponse<List<StudentWithSchoolDto> >findAllStudent(Long studentId);
+
+    ApiResponse<List<StudentWithSchoolDto>>getAllStudentWithSchoolId(Long studentId);
 }
