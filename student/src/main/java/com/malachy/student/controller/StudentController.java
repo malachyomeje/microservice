3251810aca5d87f2b84ce24c13db946181ef16eb
malachy <malachyomeje@gmail.com>
2023-10-07 00:02:21 +0100

    gateway

--- student/src/main/java/com/malachy/student/controller/StudentController.java
+++ student/src/main/java/com/malachy/student/controller/StudentController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/students")
+@RequestMapping("api/v1/students")
 public class StudentController {
 private final StudentService studentService;
 
